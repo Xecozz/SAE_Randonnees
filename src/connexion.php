239146a4168f638a0d94b9ecb4de0e98fc6cc335
gille231 <22301465@etu.unicaen.fr>
2024-06-10 13:18:10 +0200

    connexion system

--- src/connexion.php
+++ src/connexion.php
@@ -1,112 +1,25 @@
 <?php
-include_once "vendor/bdd_connexion/pdo_agile.php";
-include_once "vendor/bdd_connexion/param_connexion_etu.php";
-echo '<meta charset="utf-8"> ';
-// décommenter en fonction du serveur de BDD utilisé
-//define ("MOD_BDD","MYSQL");
-define("MOD_BDD", "ORACLE");
+session_start();
 
-if (MOD_BDD == "MYSQL") {
-    $db_username = $db_usernameMySQL;
-    $db_password = $db_passwordMySQL;
-    $db = $dbMySQL;
-} else {
-    $db_username = $db_usernameOracle;
-    $db_password = $db_passwordOracle;
-    $db = $dbOracle;
-}
+require_once "vendor/bdd_connexion/param_connexion_etu.php";
+require_once "vendor/bdd_connexion/connexion_pdo_etu.php";
+echo '<meta charset="utf-8"> ';
+
+$db_username = $db_usernameOracle;
+$db_password = $db_passwordOracle;
+$db = $dbOracle;
 
 $conn = OuvrirConnexionPDO($db, $db_username, $db_password);
 
-//fonction utile
-function insererDonnee($c, $sql)
-{
-    afficherObj($sql);
-    $res = majDonneesPDO($c, $sql);
-    echo "Résultats de la requête ", $res . "<br/>";
-}
-
-function corrigerDonnees($c, $sql)
-{
-    afficherObj($sql);
-    $res = majDonneesPDO($c, $sql);
-    echo "Résultats de la requête " . $res . "<br/>";
-}
-
-function lireDonnees($c, $sql)
-{
-    $tab = array();
-    $donnee = LireDonneesPDO1($c, $sql, $tab);
-
-    foreach ($tab as $v) {
-        foreach ($v as $cle => $a) {
-            echo $cle . " " . $a . " ";
-        }
-        echo "<br/>";
-    }
-    return $donnee;
-}
-
-function afficherObj($obj)
-{
-    echo "<PRE>";
-    print_r($obj);
-    echo "</PRE>";
-}
-
 //verification connexion
-
-
-function getNum($sql, $conn)
+function verify($sql, $conn, &$tab)
 {
-    $tab = array();
-    LireDonneesPDO1($conn, $sql, $tab);
-    afficherObj($tab);
-    return $tab["per_num"];
-}
-
-function verify($sql, $conn)
-{
-    $tab = array();
     LireDonneesPDO1($conn, $sql, $tab);
     if (empty($tab)) {
         return false;
     }
     return true;
 }
-
-function IfOrga($conn, $num)
-{
-    $tab = array();
-    $sql = "SELECT * FROM alp_organisateur WHERE per_num = '$num'";
-    LireDonneesPDO1($conn, $sql, $tab);
-    if (empty($tab)) {
-        return false;
-    }
-    return true;
-}
-function IfClient($conn, $num)
-{
-    $tab = array();
-    $sql = "SELECT * FROM alp_client WHERE per_num = '$num'";
-    LireDonneesPDO1($conn, $sql, $tab);
-    if (empty($tab)) {
-        return false;
-    }
-    return true;
-}
-function IfGuide($conn, $num)
-{
-    $tab = array();
-    $sql = "SELECT * FROM alp_guide WHERE per_num = '$num'";
-    LireDonneesPDO1($conn, $sql, $tab);
-    if (empty($tab)) {
-        return false;
-    }
-    return true;
-}
-
-
 if ($conn) {
     echo ("<hr/> Connexion réussie à la base de données <br/>");
 
@@ -116,19 +29,17 @@ if ($conn) {
 
         $sql = "SELECT * FROM alp_personne WHERE per_courriel = '$courriel' and per_mdp = '$password'";
 
+        $tab = array();
         echo $sql;
-        if (verify($sql, $conn) == true) {
-            $num = getNum($sql, $conn);
-            session_start();
-            $_SESSION['user_id'] = $user['id'];
-            echo "reussi";
+        if (verify($sql, $conn, $tab)) {
+            $num = $tab[0]['PER_NUM'];
+
+            $_SESSION['user_id'] = $num;
+
+            header('Location: choice_connexion.php');
         } else {
-            echo "Mauvais id";
+            header('Location: connexion.html');
         }
     }
 } else
     echo ("<hr/> Connexion impossible à la base de données <br/>");
-
-
- //   
-?>
--- src/vendor/bdd_connexion/param_connexion_etu.php
+++ src/vendor/bdd_connexion/param_connexion_etu.php
@@ -1,13 +1,9 @@
 <?php
-	// E.Porcq : TP2 PHP Exo1 et Exo2
-	// préparation SAE 2.456 : paramètres de connexion à un SGBD Oracle ou MySQL
-	// param_connexion_etu.php 29/05/2021
-
 	$db_usernameOracle = "agile_6";
 	$db_passwordOracle = "agile_6"; // 
 	$dbOracle = "oci:dbname=harpagon.unicaen.fr:1521/info.harpagon.unicaen.fr;charset=AL32UTF8";  
 
-	$db_usernameMySQL = "ETU1_4";
-	$db_passwordMySQL = "ETU1_4"; //
+	$db_usernameMySQL = "agile_6";
+	$db_passwordMySQL = "agile_6"; //
 	$dbMySQL = "mysql:host=localhost;dbname=TP2_PHP;charset=UTF8";
  ?>
