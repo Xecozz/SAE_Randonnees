26052d18aa9231e6b0557ee8b11afff8da59c310
gille231 <22301465@etu.unicaen.fr>
2024-06-10 09:38:40 +0200

    init

---
+++ vendor/bdd_connexion/param_connexion_etu.php
@@ -0,0 +1,13 @@
+<?php
+	// E.Porcq : TP2 PHP Exo1 et Exo2
+	// préparation SAE 2.456 : paramètres de connexion à un SGBD Oracle ou MySQL
+	// param_connexion_etu.php 29/05/2021
+
+	$db_usernameOracle = "ETU1_4";
+	$db_passwordOracle = "ETU1_4"; // 
+	$dbOracle = "oci:dbname=harpagon.unicaen.fr:1521/info.harpagon.unicaen.fr;charset=AL32UTF8";  
+
+	$db_usernameMySQL = "ETU1_4";
+	$db_passwordMySQL = "ETU1_4"; //
+	$dbMySQL = "mysql:host=localhost;dbname=TP2_PHP;charset=UTF8";
+ ?>
