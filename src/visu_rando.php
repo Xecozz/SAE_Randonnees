4d6cdd45112df457cc65d1b16f0c36e8b10c6134
Martin Bazoge <martin.bazoge@etu.unicaen.fr>
2024-06-10 13:28:21 +0200

    truc

--- src/visu_rando.php
+++ src/visu_rando.php
@@ -1,56 +1,47 @@
 <?php
-	// XXX : TP2 PHP Exo1 et Exo2
-	// préparation SAE 2.456 : programme principal
-	// connexion_oracle_etu.php 29/05/2021
-	
 	include_once "vendor/bdd_connexion/pdo_agile.php";
 	include_once "vendor/bdd_connexion/param_connexion_etu.php";
 	echo '<meta charset="utf-8"> ';
-	// décommenter en fonction du serveur de BDD utilisé
 	define ("MOD_BDD","ORACLE");
-
+	
 	$db_username = $db_usernameOracle;		
 	$db_password = $db_passwordOracle;	
 	$db = $dbOracle;
 	
 	$conn = OuvrirConnexionPDO($db,$db_username,$db_password);
-
-	afficherObj($conn);
-
+	
 	if ($conn)
 	{
 		echo ("<hr/> Connexion réussie à la base de données <br/>");
 		$table = lireDonnees($conn);
 		afficherObj($table);
 	}
-	else
-		echo ("<hr/> Connexion impossible à la base de données <br/>");
 	
+	//echo $cle ." " . $a . " ";
 	
-	
-	
-
 	function lireDonnees($c)
 	{
-		$sql = "select * from alp_randonnee";
+		$sql = "select niv_code,per_num_guide,per_num_orga,ran_nom,ran_date_d,ran_date_fin,res_prix_pers,res_sup_solo,res_descriptif from alp_randonnee";
 		$tab =array();
 		$donnee = LireDonneesPDO1($c,$sql, $tab);
+		$cpt = 0;
+		$tab2 = array();
 
 		foreach($tab as $v){
 			foreach($v as $cle=>$a){
-				echo $cle ." " . $a . " ";
+				$tab2[$cpt] = $a;
+				$cpt++;
 			}
 			echo "<br/>";
+			echo "$tab2[8]";
+			$cpt = 0;
 		}
 
 		afficherObj($tab);
 		return $donnee;
 	}
 	
-	function afficherObj($obj)
-	{
-		echo "<PRE>";
-		print_r($obj);
-		echo "</PRE>";
-	}
+/*"select niv_code,per_num_guide,per_num_orga,ran_nom,ran_date_d,
+		ran_date_fin,res_prix_pers,res_sup_solo,res_descriptif from alp_randonnee"*/
  ?>
+
