3cd7c19564eddbe0cac6c4c29146a44d8cf2afd9
Julien Moisson <moisson232@campus.unicaen.fr>
2024-06-10 13:13:51 +0200

    verif non null

--- src/traitement_formulaireRando.php
+++ src/traitement_formulaireRando.php
@@ -26,66 +26,102 @@
 	// affichage brut des éléments du formulaire
 	afficherObj($_POST);
 
+	$erreur = false;
 	
 	// il faut vérifier que ces données ont été saisies
 	if (empty($_POST["nomRando"]) ){
 		afficherObj("Veuillez mettre un nom de randonnée");
+		$erreur == true;
+	}else{
+		$nomRando = $_POST["nomRando"];
 	}
 
 	if (empty($_POST["niveau"]) || $_POST["niveau"] <= 0 || $_POST["niveau"] > 6 ){
 		afficherObj("Merci de rentrée un niveau valide");
+		$erreur == true;
+	}else {
+		$niveau = $_POST["niveau"];
 	}
 	
 	if (empty($_POST["stationDep"]) ){
-		afficherObj("Veuillez mettre un nom de qtation de départ");
+		afficherObj("Veuillez mettre un nom de station de départ");
+		$erreur == true;
+	}else{
+		$station = $_POST["stationDep"];
 	}
+
+
 	if (empty($_POST["dateDep"]) ){
 		afficherObj("Veuillez mettre une date de depart");
+		$erreur == true;
+	}else{
+		$dateDep = $_POST["dateDep"];
 	}
+
 	if (empty($_POST["stationFin"]) ){
 		afficherObj("Veuillez mettre un nom de station d'arrivée");
+		$erreur == true;
+	}else {
+		$station = $_POST["stationFin"];
 	}
-	if (empty($_POST["dateFin"]) ){
+
+	if (empty($_POST["dateFin"])){
 		afficherObj("Veuillez mettre une date de fin");
+		$erreur == true;
+	}else {
+		$dateFin = $_POST["dateFin"];
 	}
-	if (empty($_POST["stationDep"]) ){
-		afficherObj("Veuillez mettre un nom de randonnée");
+
+	if (empty($_POST["prixPers"]) || $_POST["prixPers"] < 0 ){
+		afficherObj("Veuillez mettre un prix");
+		$erreur == true;
+	}else {
+		$prixPers = $_POST["prixPers"];
 	}
-	if (empty($_POST["stationDep"]) ){
-		afficherObj("Veuillez mettre un nom de randonnée");
+
+	if (empty($_POST["supUnePers"])  || $_POST["supUnePers"] < 0  ){
+		afficherObj("Veuillez mettre un prix");
+		$erreur == true;
+	}else {
+		$supUnePers = $_POST["supUnePers"];
 	}
-	if (empty($_POST["stationDep"]) ){
-		afficherObj("Veuillez mettre un nom de randonnée");
-	}
-	if (empty($_POST["stationDep"]) ){
-		afficherObj("Veuillez mettre un nom de randonnée");
-	}
-	
 
 
-	
-
-	
-	/*
-
-	$gouts = $_POST["gouts"]; // non obligatoire
 	
 	// calcul (simpliste) du numéro de personne
-	$sql = "select nvl(max(per_num),0) as maxi from personne";
+	$sql = "select nvl(max(ran_num),0) as maxi from ALP_RANDONNEE";
 	LireDonneesPDO2($conn,$sql,$donnee);  
 	afficherObj($donnee);
-	$per_num = $donnee[0]['MAXI'] + 1;
+	$ran_num = $donnee[0]['MAXI'] + 1;
 
+	//verification que le guide existe si il y a
+	if(!empty($_POST["nomGuide"]) && !empty($_POST["prenomGuide"])){
+		$nomGide = $_POST["nomGuide"];
+		$prenomGuide = $_POST["prenomGuide"];
+		
+		$sql = "select count(*) as nb from ALP_PERSONNE
+		join ALP_GUIDE using(per_num)
+		where upper(per_nom) = upper($nomGide)
+		and upper(per_prenom) = upper($prenomGuide)";
+		LireDonneesPDO2($conn,$sql,$donnee);  
+		afficherObj($donnee);
+		$ran_num = $donnee[0]['MAXI'] + 1;
+	}
+	
+
+	$_SESSION["per_num"];
+	
 	if (	$erreur == false )
 	{	
-		$sql = "INSERT INTO personne VALUES ($per_num,'$nom','".$prenom."','".$code."','".$genre."','".$pays."','".$gouts."')";
+		$sql = "INSERT INTO alp_randonnee VALUES ($ran_num,'$niveau','".$prenom."','".$code."','".$genre."','".$pays."','".$gouts."')";
 		afficherObj($sql);
+	}
+	
+		/*
 		$res = majDonneesPDO($conn,$sql);
 		echo "Résultats de la requête ",$res . "<br/>";
 		afficherObj($res);
 	}
-	else
-		afficherObj("Le formulaire n'est pas complet");
 
 
 	// partie 2
@@ -102,6 +138,7 @@
 		}
 	}		
 
-*/
+	*/
+
 
 ?>
