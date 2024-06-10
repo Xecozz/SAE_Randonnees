7a44ee1c0b2cec141e5d3aa02d635ae7527823b2
Martin Bazoge <martin.bazoge@etu.unicaen.fr>
2024-06-10 13:30:13 +0200

    modif

--- vendor/bdd_connexion/param_connexion_etu.php
+++ vendor/bdd_connexion/param_connexion_etu.php
@@ -3,8 +3,8 @@
 	// préparation SAE 2.456 : paramètres de connexion à un SGBD Oracle ou MySQL
 	// param_connexion_etu.php 29/05/2021
 
-	$db_usernameOracle = "ETU1_4";
-	$db_passwordOracle = "ETU1_4"; // 
+	$db_usernameOracle = "agile_6";
+	$db_passwordOracle = "agile_6"; // 
 	$dbOracle = "oci:dbname=harpagon.unicaen.fr:1521/info.harpagon.unicaen.fr;charset=AL32UTF8";  
 
 	$db_usernameMySQL = "ETU1_4";
